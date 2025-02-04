0b0df421633f135be1476eda3523e7354bb9b4d0
meghanaasunil <meghanaa.sunil@gmail.com>
2025-02-04 16:18:10 +0530

    web proj

---
+++ forever/forever/frontend/src/components/Footer.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <div>
+      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+
+        <div>
+          <img className='mb-5 w-32' src={assets.logo} alt="" />
+          <p className='w-full md:w-2/3 text-gray-600'>In the 1500s, clothing was elegant and whimsical. Women wore embroidered gowns with full skirts and detachable sleeves, while men donned padded doublets, puffed breeches, and feathered hats. Lace ruffs framed faces like snowflakes, and luxurious fabrics like silk and velvet showcased status and personality in every carefully crafted detail..</p>
+        </div>
+
+        <div>
+          <p className='text-xl font-medium mb-5'>COMPANY</p>
+          <ul className='flex flex-col gap-1 text-gray-600'>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+
+        <div>
+          <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+          <ul className='flex flex-col gap-1 text-gray-600'>
+            <li>+1-212-456-7890</li>
+            <li>Contact@foreveryou.com</li>
+          </ul>
+        </div>
+
+      </div>
+
+      <div>
+        <hr />
+        <p className='py-5 text-sm text-center'>Copyright 2024@ meshda.com - All Right Reserved.</p>
+      </div>
+
+    </div>
+  )
+}
+
+export default Footer
