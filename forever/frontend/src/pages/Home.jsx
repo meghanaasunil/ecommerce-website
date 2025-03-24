a3038879784538ef26b1023b5f12f770efe98398
Shrav-20 <shravya.v2023@vitstudent.ac.in>
2025-03-24 14:49:43 +0530

    Update Home.jsx

--- forever/frontend/src/pages/Home.jsx
+++ forever/frontend/src/pages/Home.jsx
@@ -14,6 +14,32 @@ const Home = () => {
       <OurPolicy />
       <NewsletterBox />
     </div>
+    <script src='https://cdn.jotfor.ms/s/umd/latest/for-embedded-agent.js'></script>
+<script>
+  window.addEventListener("DOMContentLoaded", function() {
+    window.AgentInitializer.init({
+      agentRenderURL: "https://agent.jotform.com/0195c76e35ae7f649148f3eb09f6de642434",
+      rootId: "JotformAgent-0195c76e35ae7f649148f3eb09f6de642434",
+      formID: "0195c76e35ae7f649148f3eb09f6de642434",
+      queryParams: ["skipWelcome=1", "maximizable=1"],
+      domain: "https://www.jotform.com",
+      isDraggable: false,
+      background: "linear-gradient(180deg, #6C73A8 0%, #6C73A8 100%)",
+      buttonBackgroundColor: "#0066C3",
+      buttonIconColor: "#FFFFFF",
+      variant: false,
+      customizations: {
+        "greeting": "Yes",
+        "greetingMessage": "Hi! How can I assist you?",
+        "openByDefault": "No",
+        "pulse": "Yes",
+        "position": "right",
+        "autoOpenChatIn": "0"
+      },
+      isVoice: undefined
+    });
+  });
+</script>
   )
 }
 
